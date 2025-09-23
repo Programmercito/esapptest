e008156862a627f4c891d5fc137c39db97dcd95a
programmercito <osboceo@live.com>
2025-09-23 16:59:30 -0400

    new components

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 import { AppLayout } from './app/layout/component/app.layout';
 import { Transaction } from '@/transaction/component/transaction';
+import { Dashboard } from '@/dashboard/component/dashboard';
+import { History } from '@/history/component/history';
 
 export const appRoutes: Routes = [
     {
         path: '',
         component: AppLayout,
         children: [
-            { path: '', component: Transaction },
+            { path: 'transaction', component: Transaction },
+            { path: 'history', component: History },
+            { path: '', component: Dashboard },
         ]
     }
 ];
